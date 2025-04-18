aeb39fe98ce3cdade9f210e586c54cf435e16d95
adachi-kazuma <115063024+adachi-kazuma@users.noreply.github.com>
2025-04-18 16:53:17 +0900

    01の課題

--- main.cpp
+++ main.cpp
@@ -1,11 +1,24 @@
 #include <stdio.h>
-#include <iostream>
-#include <Windows.h>
+template <typename Type>
 
+
+Type Min(Type a, Type b)
+{
+	if (a > b) {
+		return static_cast<Type> (b);
+	}
+	return a;
+}
+template <>
+char Min<char>(char a, char b) {
+	printf("数字以外は代入出来ません\n");
+	return a;
+}
 int main()
 {
-	char str[] = "ソs品";
-	printf("%s",str);
-
+	printf("%d\n", Min<int>(100, 10));
+	printf("%f\n", Min<float>(1.9f, 1.0f));
+	printf("%If\n", Min<double>(12, 11));
+	Min<char>('a', 'b');
 	return 0;
 }
