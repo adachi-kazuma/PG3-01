dbfb836206b2991ec3167b35a8ae28d5507fe18e
adachi-kazuma <115063024+adachi-kazuma@users.noreply.github.com>
2025-05-09 15:20:29 +0900

    課題

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <stdio.h>
-template <typename Type>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>  
 
-int main()
-{
-	printf("Hello");
 
-	return 0;
+int randum(int dice, int guess) {
+    return (dice % 2) == guess;
+}
+
+void saikoro(int dice, int guess, int (*callback)(int, int)) {
+    printf("判定中...\n");
+    Sleep(3000);  // 3秒
+    if (callback(dice, guess)) {
+        printf("正解。\n", dice);
+    }
+    else {
+        printf("不正解。\n", dice);
+    }
+}
+
+int main() {
+    int num;
+    srand((unsigned)time(NULL)); 
+
+    printf("半(1=奇数) or 丁(0=偶数) を選んでください: ");
+    scanf_s("%d", &num);
+
+    int dice = rand() % 6 + 1;
+    saikoro(dice, num, randum);
+
+    return 0;
 }
