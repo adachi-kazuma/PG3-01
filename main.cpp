8597505bf6325fdad38213cd3e495fc3e9378bcc
adachi-kazuma <115063024+adachi-kazuma@users.noreply.github.com>
2025-05-13 11:58:28 +0900

    完了

--- main.cpp
+++ main.cpp
@@ -3,7 +3,6 @@
 #include <time.h>
 #include <windows.h>  
 
-
 int randum(int dice, int guess) {
     return (dice % 2) == guess;
 }
@@ -22,10 +21,8 @@ void saikoro(int dice, int guess, int (*callback)(int, int)) {
 int main() {
     int num;
     srand((unsigned)time(NULL)); 
-
     printf("半(1=奇数) or 丁(0=偶数) を選んでください: ");
     scanf_s("%d", &num);
-
     int dice = rand() % 6 + 1;
     saikoro(dice, num, randum);
 
