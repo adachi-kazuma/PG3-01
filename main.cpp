f7db4f824c92f4587f7a4987e90f6f201fb05a45
adachi-kazuma <115063024+adachi-kazuma@users.noreply.github.com>
2025-05-23 14:06:02 +0900

    2_01

--- main.cpp
+++ main.cpp
@@ -1,57 +1,59 @@
-#include<stdio.h>
-#include<Windows.h>
-#include <time.h>
-#include <functional>
+#include <iostream>
+using namespace std;
 
+class Enemy {
+public:
+    void Update();
 
-//コールバック関数
-void DispResult(int* s, int* kye) {
-	int dice = rand() % 2;
+    void Approach(); // 接近
+    void Attack();   // 攻撃
+    void Retreat();  // 離脱
 
-	if (dice == *kye) {
-		if (dice == 0)
-			printf("%dで丁(偶数)でした。当たり", dice);
-		else
-			printf("%dで半(奇数)でした。当たり", dice);
-	}
-	else {
-		if (dice == 1)
-			printf("%dで半(奇数)でした。はずれ", dice);
-		else
-			printf("%dで丁(偶数)でした。はずれ", dice);
-	}
+    // 関数ポインタテーブル
+    static void (Enemy::* table[])();
 
-}
-
-void setTimeout(std::function<void(int*, int*)>p, int second, int kye) {
-	//コールバック関数を呼び出す
-	for (int i = 0; i < second; i++) {
-		Sleep(1000);
-		printf("%d...\n", second - i);
-	}
-
-
-	p(&second, &kye);
-}
-
-int main() {
-
-	int kye;
-
-	srand(static_cast<unsigned int>(time(NULL)));
-	printf("丁(偶数)なら0、半(奇数)なら1を打つ\n");
-	scanf_s("%d", &kye);
-
-	if (kye == 0) {
-		puts("あなたは丁(偶数)を選びました");
-	}
-	else {
-		puts("あなたは半(奇数)を選びました");
-	}
-
-	std::function<void(int*, int*)> p = [](int* s, int* kye) { DispResult(s, kye); };
-	setTimeout(p, 3, kye);
-
-	return 0;
+private:
+    int index = 0;
 };
 
+void Enemy::Approach() {
+    cout << "敵が接近！" << endl;
+}
+
+void Enemy::Attack() {
+    cout << "敵が攻撃！" << endl;
+}
+
+void Enemy::Retreat() {
+    cout << "敵が離脱！" << endl;
+}
+
+void Enemy::Update() {
+    // 関数テーブルから関数を実行
+    (this->*table[index])();
+
+    cout << "次の状態に移行しますか？ (0: はい、 他: いいえ): ";
+    int input;
+    cin >> input;
+
+    if (input == 0) {
+        index = (index + 1) % 3; // 状態を循環
+    }
+}
+
+// メンバ関数ポインタテーブルの初期化
+void (Enemy::* Enemy::table[])() = {
+    &Enemy::Approach,
+    &Enemy::Attack,
+    &Enemy::Retreat
+};
+
+int main() {
+    Enemy enemy;
+
+    while (true) {
+        enemy.Update();
+    }
+
+    return 0;
+}
