93c83320b31c1a6ae24e2043a6c62e3f13456a16
adachi-kazuma <115063024+adachi-kazuma@users.noreply.github.com>
2025-05-23 14:02:18 +0900

    課題

--- main.cpp
+++ main.cpp
@@ -1,30 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<Windows.h>
 #include <time.h>
-#include <windows.h>  
+#include <functional>
 
-int randum(int dice, int guess) {
-    return (dice % 2) == guess;
+
+//コールバック関数
+void DispResult(int* s, int* kye) {
+	int dice = rand() % 2;
+
+	if (dice == *kye) {
+		if (dice == 0)
+			printf("%dで丁(偶数)でした。当たり", dice);
+		else
+			printf("%dで半(奇数)でした。当たり", dice);
+	}
+	else {
+		if (dice == 1)
+			printf("%dで半(奇数)でした。はずれ", dice);
+		else
+			printf("%dで丁(偶数)でした。はずれ", dice);
+	}
+
 }
 
-void saikoro(int dice, int guess, int (*callback)(int, int)) {
-    printf("判定中...\n");
-    Sleep(3000);  // 3秒
-    if (callback(dice, guess)) {
-        printf("正解。\n", dice);
-    }
-    else {
-        printf("不正解。\n", dice);
-    }
+void setTimeout(std::function<void(int*, int*)>p, int second, int kye) {
+	//コールバック関数を呼び出す
+	for (int i = 0; i < second; i++) {
+		Sleep(1000);
+		printf("%d...\n", second - i);
+	}
+
+
+	p(&second, &kye);
 }
 
 int main() {
-    int num;
-    srand((unsigned)time(NULL)); 
-    printf("半(1=奇数) or 丁(0=偶数) を選んでください: ");
-    scanf_s("%d", &num);
-    int dice = rand() % 6 + 1;
-    saikoro(dice, num, randum);
 
-    return 0;
-}
+	int kye;
+
+	srand(static_cast<unsigned int>(time(NULL)));
+	printf("丁(偶数)なら0、半(奇数)なら1を打つ\n");
+	scanf_s("%d", &kye);
+
+	if (kye == 0) {
+		puts("あなたは丁(偶数)を選びました");
+	}
+	else {
+		puts("あなたは半(奇数)を選びました");
+	}
+
+	std::function<void(int*, int*)> p = [](int* s, int* kye) { DispResult(s, kye); };
+	setTimeout(p, 3, kye);
+
+	return 0;
+};
+
