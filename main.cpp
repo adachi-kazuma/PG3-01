c5b2a860c5211944030c3ef27cef19743c172e41
adachi-kazuma <115063024+adachi-kazuma@users.noreply.github.com>
2025-07-25 15:31:10 +0900

    1

--- main.cpp
+++ main.cpp
@@ -1,8 +1,20 @@
-#include <stdio.h>
+#include <iostream>
+#include <thread>
 
-int main()
-{
-	printf("Hello");
+using namespace std;
 
-	return 0;
+void PrintThread(uint32_t num) {
+    cout << "thread" << num << endl;
+}
+
+int main() {
+    thread t1(PrintThread, 1);
+    thread t2(PrintThread, 2);
+    thread t3(PrintThread, 3);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
 }
